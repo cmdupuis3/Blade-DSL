2239eb3cae56354be618b860ecf71b3ca527bd26
Christopher Dupuis <45972964+cmdupuis3@users.noreply.github.com>
2019-06-07 13:59:35 -0400

    Update sketches.fsx

--- fs/sketches.fsx
+++ fs/sketches.fsx
@@ -17,15 +17,14 @@ type Counter() =
         num <- num + 1
         String.concat "" ["__i"; (string num)]
 
-let counter = Counter()
-let loop (imin: int) (imax: int) =
-    loopText "int" counter.Next imin imax
+let loop (imin: int) (imax: int) (ctr: Counter) =
+    loopText "int" ctr.This imin imax
 
 let index i arrayName =
     String.concat "" [arrayName; "("; (string i); ")"]
 
-let indexToTemp i arrayName =
-    String.concat "" [arrayName; counter.This; "("; (string i); ") = "; (index i arrayName); ";"]
+let indexToTemp i arrayName (ctr: Counter) =
+    String.concat "" ["auto "; arrayName; string (ctr.Next); " = "; (index i arrayName); ";"]
 
 let tab x = 
     x |> List.map (fun y -> String.concat "" ["\t"; y])
@@ -37,16 +36,19 @@ let brace x =
     List.append [String.concat " " [x; "{\n"]] ["\n}"]
 
 
-let rec loopNestRec (extents: int list) (inner: string list) =
+let rec loopNestRec (arrayName: string) (extents: int list) (inner: string list) (ctr: Counter) =
     match extents with
     | [] -> inner
     | head::tail -> (
-        let braced = brace (loop 0 head)
-        List.append (List.append [braced.[0]] (tab (loopNestRec tail inner))) [braced.[1]]
+        let braced = brace (loop 0 head ctr)
+        List.concat [ [braced.[0]];
+                      tab [(indexToTemp ctr.This arrayName ctr)];
+                      tab (loopNestRec arrayName tail inner ctr);
+                      [braced.[1]] ]
     )
 
-            
 
+let c = Counter()
 
 
 
