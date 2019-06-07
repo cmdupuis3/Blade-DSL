342120dc8b1e47cfd0ffcfa1ff66cdd692f1e29b
Christopher Dupuis <45972964+cmdupuis3@users.noreply.github.com>
2019-06-07 12:25:55 -0400

    F#: now generating correct nested loop string lists

--- fs/sketches.fsx
+++ fs/sketches.fsx
@@ -6,19 +6,26 @@ open iterators
 
 // Define your library scripting code here
 
-let generate_for itype iname (imin: int) (imax: int) =
+let loopText itype iname (imin: int) (imax: int) =
     String.concat "" ["for("; itype; " "; iname; " = "; (string imin); "; "; iname; " < "; (string imax); "; "; iname; "++)";]
         
 type Counter() =
     let mutable num = 0
-    member this.next = 
+    member this.This = 
+        String.concat "" ["__i"; (string num)]
+    member this.Next = 
         num <- num + 1
         String.concat "" ["__i"; (string num)]
 
-let counter = new Counter()
+let counter = Counter()
 let loop (imin: int) (imax: int) =
-    generate_for "int" counter.next imin imax
+    loopText "int" counter.Next imin imax
 
+let index i arrayName =
+    String.concat "" [arrayName; "("; (string i); ")"]
+
+let indexToTemp i arrayName =
+    String.concat "" [arrayName; counter.This; "("; (string i); ") = "; (index i arrayName); ";"]
 
 let tab x = 
     x |> List.map (fun y -> String.concat "" ["\t"; y])
@@ -27,16 +34,27 @@ let newln x =
     x |> List.map (fun y -> String.concat "" ["\n"; y])
     
 let brace x = 
-     List.append (List.append ["{\n"] x) ["\n}"]
+    List.append [String.concat " " [x; "{\n"]] ["\n}"]
 
-let nest_for loop inner =
-    List.append [loop] ((brace << tab) inner)
+
+let rec loopNestRec (extents: int list) (inner: string list) =
+    match extents with
+    | [] -> inner
+    | head::tail -> (
+        let braced = brace (loop 0 head)
+        List.append (List.append [braced.[0]] (tab (loopNestRec tail inner))) [braced.[1]]
+    )
+
+            
 
 
 
 
 let myfunc = ["iarray.read();"; "oarray = iarray;"; "oarray.write();"]
 
-let a = generate_for "int" "i" 0 10
-let b = generate_for "int" "j" 0 20
+let a = loop 0 10
+let b = loop 0 20
 
+
+let iarray = "iarray"
+let oarray = "oarray"
