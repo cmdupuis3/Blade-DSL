e271672b19c5bdc9e4b7a6b8702a7d7c9ad0789b
Christopher Dupuis <45972964+cmdupuis3@users.noreply.github.com>
2019-06-20 13:34:27 -0400

    tokenizer and syntax sketch

--- fs/parser.fsx
+++ fs/parser.fsx
@@ -1,7 +1,71 @@
 
+
 open System.IO
+open System.Text.RegularExpressions
 
 #load "parser.fs"
+
+type Token = 
+    | WhiteSpace
+    | Symbol of char
+    | Str of string
+    | Int of int
+
+let (|Match|_|) pattern input =
+    let m = Regex.Match (input, pattern)
+    if m.Success then Some m.Value else None
+
+let toToken = function
+    | Match @"^\s+"                  s -> s, Token.WhiteSpace
+    | Match @"^\{|^\}|^\(|^\)|^,"    s -> s, Token.Symbol s.[0]
+    | Match @"^[a-zA-Z][a-zA-Z0-9]*" s -> s, Token.Str s
+    | Match @"^\d+"                  s -> s, Token.Int (int s)
+    | _ -> failwith "Invalid Token"
+
+
+let tokenize (s: string) =
+    let rec tokenize' index (s: string) =
+        if index = s.Length then []
+        else
+            let text, token = toToken (s.Substring index)
+            token :: tokenize' (index + text.Length) s
+    tokenize' 0 s
+    |> List.choose (function Token.WhiteSpace -> None | t -> Some t)
+
+
+type Syntax =
+    | Block of string
+    | Pragma of InitClause * Clause list
+    | Declaration of Declaration
+    | Assignment of Assignment
+    | Null
+and Type = string
+and Identifier = string
+and Declaration = Type * Identifier
+and Clause =
+    | NullClause of string
+    | StrClause of string * string
+    | IntClause of string * int
+    | StrListClause of string * string list
+    | IntListClause of string * int list
+and InitClause =
+    | NullClause of string
+    | StrClause of string * string
+and Assignment = 
+    | Assign of Identifier * Expression
+    | Construct of Declaration * Expression
+and Expression =
+    | MethodLoopInit of string * Identifier list
+    | ObjectLoopInit of string * Identifier
+    | MethodLoopCall of string * Identifier
+    | ObjectLoopCall of string * Identifier list
+    | NestedLoopCall of string * Identifier * Identifier list
+    | Pipe of string * Identifier list
+    | Cat  of string * Identifier list
+
+
+
+
 
 
 let code = """
