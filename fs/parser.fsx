371661fddf2e98907f249783f4934a3a872bcff9
Christopher Dupuis <45972964+cmdupuis3@users.noreply.github.com>
2019-07-08 15:56:22 -0400

    Array lexing sketches

--- fs/parser.fsx
+++ fs/parser.fsx
@@ -2,9 +2,6 @@
 open System.IO
 open System.Text.RegularExpressions
 
-//#r "FParsec"
-//open FParsec
-
 #load "parser.fs"
 
 /// Union of all the relevant token types
@@ -25,7 +22,7 @@ let (|Match|_|) pattern input =
 let toToken = function
     | Match @"^\n|^\r"               s -> s, Token.NewLine
     | Match @"^\s+"                  s -> s, Token.WhiteSpace
-    | Match @"^\{|^\}|^\(|^\)|^\[|^\]|^,|^\#|^\<|^\>|^;"    s -> s, Token.Symbol s.[0]
+    | Match @"^\{|^\}|^\(|^\)|^\[|^\]|^,|^\#|^\<|^\>|^;|^:|^~|^\*|^="    s -> s, Token.Symbol s.[0]
     | Match @"^[a-zA-Z][a-zA-Z0-9]*" s -> s, Token.Str s
     | Match @"^\d+"                  s -> s, Token.Int (int s)
     | Match @"."                     s -> s, Token.Other (string s)
@@ -47,12 +44,17 @@ let tokenize (s: string) =
 type Clause = string * Token list
 
 /// Pragma scope type; contains an unparsed list of tokens visible to the pragma
-type PragmaScope =
+type BlockScope =
+    | Nested of BlockScope list
     | Block of Token list
     | Line of Token list
 
+type Scope = class
+    member this.sub = 9
+end
+
 /// Pragma type; consists of a directive, a list of clauses, and a scope
-type Pragma = Clause * Clause list * PragmaScope
+type Pragma = Clause * Clause list * BlockScope
 
 /// Pattern for pragma scopes; basically dumps all the tokens inside the pragma scope into a buffer, and returns the tail separately
 let rec (|ScopePattern|_|) = function
@@ -81,12 +83,20 @@ let rec (|ScopePattern|_|) = function
                 head' :: h', t'
             | _ -> failwith "asdfsdf"
         let scope, t = toScope tail 0
-        Some (PragmaScope.Block (scope), (t: Token list))
+        Some (BlockScope.Block (scope), (t: Token list))
     | head :: tail -> 
         match tail with
         | ScopePattern t -> Some t
         | _ -> None
     | _ -> None
+
+/// Parse code for all the scopes and return a list of them.
+let rec parseScopes (s: Token list) =
+    let rec parse' = function
+    | ScopePattern (v,t) -> v :: (parse' t)
+    | head :: tail -> parse' tail
+    | [] -> []
+    BlockScope.Nested (parse' s)
 
 //match tokenize "asd(8) {fjfj jfjf }\n" with | ScopePattern(s) -> Some(s) |_->None;;
 //let a = match (tokenize "asd(8) {fjfj jfjf { 9 {a b c} 7 8 } }\n asdf {9 8}\n") with | ScopePattern v -> Some(v) |_-> None ;;
@@ -95,7 +105,7 @@ let rec (|ScopePattern|_|) = function
 /// Pattern for pragmas applied to individual lines; basically dumps all the tokens inside the pragma scope into a buffer, and returns the tail separately
 let rec (|LinePattern|_|) = function
     | head :: Token.Symbol ';' :: Token.NewLine :: tail | head :: Token.Symbol ';' :: tail ->
-        Some ([head], tail)
+        Some (head :: [Token.Symbol ';'], tail)
     | head :: tail ->
         let h, t = 
             match tail with
@@ -140,40 +150,113 @@ let (|PragmaPattern|_|) = function
     | Token.Symbol '#' :: Token.Str "pragma" :: Token.Str "edgi" :: ClausesPattern (cl, Token.NewLine :: tail) ->
         match tail with
         | ScopePattern (h, t) -> h, t
-        | LinePattern (h, t) -> PragmaScope.Line h, t
+        | LinePattern (h, t) -> BlockScope.Line h, t
         | _ -> failwith "edgi pragma must occur before a statement or a block."
         ||> fun h -> fun t ->
             Some (Pragma (cl.Head, cl.Tail, h), t)
     | _ -> None
 
+
+type Statement =
+    | MethodLoopInit of Token * Token list
+    | ObjectLoopInit of Token * Token
+    | MethodLoopCall of Token * Token
+    | ObjectLoopCall of Token * Token list
+    | NestedLoopCall of Token * Token * Token list
+    | Pipe of Token * Token list
+    | Cat  of Token * Token list
+
+
+let (|StatementPattern|_|) = function
+    | Token.Str loop :: Token.Symbol '=' :: Token.Str "method_for" :: Token.Symbol '(' :: tail ->
+        let elements, t = toElements tail
+        Some (Statement.MethodLoopInit (Token.Str loop, elements), tail)
+    | Token.Str loop :: Token.Symbol '=' :: Token.Str "object_for" :: Token.Symbol '(' :: func :: Token.Symbol ')' :: tail ->
+        Some (Statement.ObjectLoopInit (Token.Str loop, func), tail)
+
+
 /// Parse code for all the pragmas and return a list of them.
-let parse (s: string) =
+let parsePragmas (s: Token list) =
     let rec parse' = function
     | PragmaPattern (v,[]) -> [v]
     | PragmaPattern (v, t) -> v :: (parse' t)
     | head :: tail -> parse' tail
     | [] -> []
-    parse' (tokenize s)
+    parse' s
+
+type NestedArray = 
+    { arrName: string
+      arrType: string
+      arrRank: int 
+      arrSym:  int list list}
+
+type NestedNetCDFArray = 
+    { arrName:  string
+      arrType:  string
+      arrRank:  int
+      arrSym:   int list list
+      fileName: string
+      varName:  string }
 
 
-type Type = string
+type NestedClosure(iarraysIn: NestedArray list, symGroupsIn: int list list, comGroupsIn: int list, oarraysIn: NestedArray list, functionIn: BlockScope) =
+    member this.Iarrays = iarraysIn
+    member this.SymGroups = symGroupsIn
+    member this.ComGroups = comGroupsIn
+    member this.Oarrays = oarraysIn
+    member this.Function = functionIn
 
-type Identifier = string
+let (|ArraySymmetryPattern|_|) = function
+    | "symmetry", (vals: Token list) -> Some (vals)
+    | _ -> failwith "Invalid array clause."
 
-type Declaration = Type * Identifier
+let getSymmetry (rank: int) (symGroups: int list) = 
+    if symGroups.IsEmpty then 
+        List.init rank id
+    else
+        if symGroups.Length = rank then
+            symGroups
+        else
+            failwith "Symmetry vector length did not match the rank of the array."
 
-type Expression =
-    | MethodLoopInit of Identifier list
-    | ObjectLoopInit of Identifier
-    | MethodLoopCall of Identifier
-    | ObjectLoopCall of Identifier list
-    | NestedLoopCall of Identifier * Identifier list
-    | Pipe of Identifier list
-    | Cat  of Identifier list
+let (|ArrayPattern|_|) (symGroups: int list) = function
+    | BlockScope.Line (Token.Str valtype :: Token.Symbol '~' :: Token.Int rank :: Token.Str name :: Token.Symbol ';' :: tail) ->
+        Some ( {arrName = name; arrType = valtype; arrRank = rank; arrSym = [getSymmetry rank symGroups]}, tail )
+    | BlockScope.Line (Token.Str "promote" :: Token.Symbol '<' :: Token.Str valtype :: Token.Symbol ',' :: Token.Int rank :: Token.Symbol '>' :: Token.Symbol ':' :: Token.Symbol ':' :: Token.Str "type" :: Token.Str name :: Token.Symbol ';' :: tail) ->
+        Some ( {arrName = name; arrType = valtype; arrRank = rank; arrSym = [getSymmetry rank symGroups]}, tail )
+    | _ -> None
 
-type Assignment = 
-    | Assign of Identifier * Expression
-    | Construct of Declaration * Expression
+let (|NetCDFArrayPattern|_|) (symGroups: int list) = function
+    | BlockScope.Line (Token.Str valtype :: Token.Symbol '~' :: Token.Int rank :: Token.Symbol '<' :: Token.Str fname :: Token.Symbol ',' :: Token.Str vname :: Token.Symbol '>' :: Token.Str name :: Token.Symbol ';' :: tail) ->
+        Some ( {arrName = name; arrType = valtype; arrRank = rank; arrSym = [getSymmetry rank symGroups]; fileName = fname; varName = vname}, tail )
+    | _ -> None
+
+
+let getArray (clauses: Clause list) (block: BlockScope) =
+    let rec findArrSym (cls: Clause list) = 
+        match cls with
+        | [] -> []
+        | _ ->
+            match cls.Head with
+            | ArraySymmetryPattern s -> s |> List.map (string >> int)
+            | _ -> findArrSym cls.Tail
+    let a = findArrSym clauses
+    match block with
+    | ArrayPattern a s -> [Some(fst s)]
+    | _ -> failwith "Array pragma applied to invalid array declaration."
+
+
+let parse (s: Token list) = 
+    let pragmas = parsePragmas s
+    let rec parse' s' =
+        match pragmas with
+        | [] -> []
+        | (pragma, clauses, block) :: tail ->
+            match fst pragma with
+            | "array" -> getArray clauses block
+            | "function" -> []
+            | _ -> failwith "Invalid pragma."
+    parse' s
 
 
 let code = """
