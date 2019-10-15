372d795b1e99e53313f7ecde70460383afdaded2
Christopher Dupuis <45972964+cmdupuis3@users.noreply.github.com>
2019-10-15 11:34:23 -0400

    parser file I/O

--- fs/parser.fsx
+++ fs/parser.fsx
@@ -1092,80 +1092,13 @@ let lex (tokens: Token list) =
     |> stringCollapse ""
 
 
+[<EntryPoint>]
+let main args =
+    let iFileName, oFileName = args.[0], args.[1]
 
+    File.ReadAllText iFileName
+    |> tokenize
+    |> lex
+    |> fun x -> File.WriteAllText (oFileName, x)
 
-let code = """
-
-#include "things.hpp"
-#include "stuff.hpp"
-#pragma edgi function(sumThenMultiply) arity(3) input(iarray1, iarray2, iarray3) iranks(1, 1, 0) commutativity(1, 1, 3) output(oarray) orank(0)
-auto sumThenMultiply = function(iarray1, iarray2, iarray3, oarray){
-    // assume iarray1 and iarray2 last extents are same
-    for(int i = 0; i < iarray1.current_extent(); i++){
-        oarray += iarray1[i] + iarray2[i];
-    }
-
-    oarray *= iarray3;
-    return oarray;
-}
-#pragma edgi function(divideThenSum) arity(3) input(iarray4, iarray5, iarray6) iranks(1, 1, 0) output(oarray) orank(0)
-auto sumThenMultiply = function(iarray4, iarray5, iarray6, oarray){
-    // assume iarray4 and iarray5 last extents are same
-    for(int i = 0; i < iarray4.current_extent(); i++){
-        oarray += iarray4[i] / iarray5[i];
-    }
-
-    oarray += iarray6;
-    return oarray;
-}
-#pragma edgi function(add10) arity(1) input(iarray) iranks(0) output(oarray) orank(0)
-{
-    oarray = iarray + 10;
-    return oarray;
-}
-#pragma edgi function(sinThenProduct) arity(any) input(iarray) iranks(0) output(oarray) orank(0)
-{
-    oarray = sin(iarray) * tail;
-    return oarray;
-}
-int main(){
-
-    #pragma edgi array symmetry(1, 2, 2, 3)
-    promote<float, 4>::type array1;
-
-    #pragma edgi array
-    promote<float, 3>::type array3;
-
-    #pragma edgi array
-    promote<float, 3>::type ooarray;
-
-    #pragma edgi array
-    promote<float, 3>::type moarray;
-
-    #pragma edgi array
-    promote<float, 3>::type moarray2;
-
-    #pragma edgi array
-    promote<float, 3>::type voarray;
-
-    auto oloop = object_for(sumThenMultiply);
-    auto ooarray = oloop(array1, array1, array3);
-
-    auto mloop = method_for(array1, array1, array3);
-    auto moarray = mloop(sumThenMultiply);
-    auto moarray2 = mloop(divideThenSum);
-
-    auto oloopv = object_for(sinThenProduct);
-    auto voarray = oloopv(array3, array3);
-
-
-
-
-    auto newfunc = pipe(sumThenMultiply, add10);
-
-    return 0;
-}"""
-
-code |> tokenize |> lex
-;;
-
+    0
