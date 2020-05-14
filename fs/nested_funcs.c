22775dc0c93c144db1146434657a708f10a7574c
Christopher Dupuis <45972964+cmdupuis3@users.noreply.github.com>
2020-05-14 15:32:13 -0400

    Update nested_funcs.c

--- fs/nested_funcs.c
+++ fs/nested_funcs.c
@@ -1,4 +1,5 @@
 
+#include <stdlib.h>
 #include "netcdf.h"
 
 extern int get_num_dims(char* file_name, char* variable_name) {
@@ -10,6 +11,8 @@ extern int get_num_dims(char* file_name, char* variable_name) {
     nc_open(file_name, NC_NOWRITE|NC_SHARE, &(file_ncid));
     nc_inq_varid(file_ncid, variable_name, &(var_ncid));
     nc_inq_varndims(file_ncid, var_ncid, &num_dims);
+
+    nc_close(file_ncid);
 
     return num_dims;
 
@@ -25,6 +28,38 @@ extern int get_var_type(char* file_name, char* variable_name) {
     nc_inq_varid(file_ncid, variable_name, &(var_ncid));
     nc_inq_vartype(file_ncid, var_ncid, &type);
 
+    nc_close(file_ncid);
+
     return (int)type;
 
 }
+
+/* This elides the difference between a dimension and a dimension variable */
+extern void get_var_dim_types(char* file_name, char* variable_name, int dim_types[]){
+
+    int file_ncid;
+    int var_ncid;
+    int num_dims = -1;
+
+    nc_open(file_name, NC_NOWRITE|NC_SHARE, &(file_ncid));
+    nc_inq_varid(file_ncid, variable_name, &(var_ncid));
+    nc_inq_varndims(file_ncid, var_ncid, &num_dims);
+
+    int var_dimids[num_dims];
+
+    nc_inq_vardimid(file_ncid, var_ncid, var_dimids);
+    for(int i = 0; i < num_dims; i++){
+        char* dimvar_name = malloc(sizeof(char) * NC_MAX_NAME);
+        int dimvar_ncid = -1;
+        nc_inq_dimname(file_ncid, var_dimids[i], dimvar_name);
+        nc_inq_varid(file_ncid, dimvar_name, &dimvar_ncid);
+        nc_inq_vartype(file_ncid, var_ncid, &(dim_types[i]));
+
+        free(dimvar_name);
+    }
+
+    nc_close(file_ncid);
+
+    return;
+}
+
