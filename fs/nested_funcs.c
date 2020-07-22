60f0bbc7390aa7ecc9d6f1eff85931803f294d80
Christopher Dupuis <45972964+cmdupuis3@users.noreply.github.com>
2020-07-22 17:29:05 -0400

    Fix dimval types

--- fs/nested_funcs.c
+++ fs/nested_funcs.c
@@ -41,19 +41,19 @@ extern void get_var_dim_types(char* file_name, char* variable_name, int dim_type
     int var_ncid;
     int num_dims = -1;
 
-    nc_open(file_name, NC_NOWRITE|NC_SHARE, &(file_ncid));
-    nc_inq_varid(file_ncid, variable_name, &(var_ncid));
+    nc_open(file_name, NC_NOWRITE|NC_SHARE, &file_ncid);
+    nc_inq_varid(file_ncid, variable_name, &var_ncid);
     nc_inq_varndims(file_ncid, var_ncid, &num_dims);
 
     int var_dimids[num_dims];
 
     nc_inq_vardimid(file_ncid, var_ncid, var_dimids);
     for(int i = 0; i < num_dims; i++){
-        char* dimvar_name = malloc(sizeof(char) * NC_MAX_NAME);
+        char* dimvar_name = (char*)malloc(sizeof(char) * NC_MAX_NAME);
         int dimvar_ncid = -1;
         nc_inq_dimname(file_ncid, var_dimids[i], dimvar_name);
         nc_inq_varid(file_ncid, dimvar_name, &dimvar_ncid);
-        nc_inq_vartype(file_ncid, var_ncid, &(dim_types[i]));
+        nc_inq_vartype(file_ncid, dimvar_ncid, &(dim_types[i]));
 
         free(dimvar_name);
     }
